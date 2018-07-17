cdbe9a257fd8b90d58453d254c9c3d7b68666b14
Jovin Thariyath <29200@ITCINFOTECH.COM>
2018-07-17 12:16:16 +0530

    Implement html, but js not working
     Committer: Jovin Thariyath <3jovin@gmail.com>

--- src/components/containers/DashboardContainer.jsx
+++ src/components/containers/DashboardContainer.jsx
@@ -19,22 +19,30 @@ class DashboardContainer extends Component {
     super();
     this.state = {
       'yellowBtn' : {'type':'yellow','size':'tiny', 'text':'Yellow'},
-      'primary' : {'type':'primary', 'text':'Primary'}
+      'primary' : {'type':'primary', 'text':'Primary'},
+      'icon' : {'text':'Icon', 'icon' : true, 'iconClass':'heart'},
+      'labeledIcon' : {'text':'Labeled Icon', 'icon' : true, 'labeled' :true, 'iconClass':'heart'},
+      'dropdown': {'text':'Dropdown','dropdown':true, 'dropdownValues': ["Test 1", "Test 2"]}
     }
   }
 
   render() {
         
-        const {yellowBtn, primary} = this.state
+        const {yellowBtn, primary, icon, labeledIcon, dropdown } = this.state
         return (
           <div className="ui two column grid">
             <div className="three wide column">
             </div>
             <div className="nine wide column">
+            <h1>Buttons</h1>
             <div className="ui divider"></div>
               <Button props = {yellowBtn} />
               <Button props = {primary} />
-            </div>
+              <Button props = {icon} />
+              <Button props = {labeledIcon} />
+              <Button props = {dropdown} />
+
+           </div>
           </div>
         )
   }
